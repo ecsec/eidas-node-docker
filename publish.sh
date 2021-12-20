728801482028d6c8a9d3e656dc65a54575db4e76
Mike Prechtl <mike.prechtl@ecsec.de>
2021-12-20 13:08:59 +0100

    Add script for helm release

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+LATEST_TAG=$(git tag -l | tail -n 1)
+
+printf "Your latest release version: ${LATEST_TAG}\n"
+read -p "What is the new release version (<EIDAS_NODE_VERSION>-<REVISION>): " NEW_TAG
+
+# Update version in helm chart
+sed -i 's@version: .*@version: '"${NEW_TAG}"'@' ${DIR}/helm/Chart.yaml
+
+# Commit, Tag and push changes
+git add ${DIR}/helm/Chart.yaml
+git commit -m "Helm Chart Release ${NEW_TAG}"
+git tag -s v${NEW_TAG} -m "v${NEW_TAG}"
+git push --atomic origin main v${NEW_TAG}
