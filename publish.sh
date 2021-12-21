54227e10bb0a549763b8a2dcf5abf9c7342d6e48
Mike Prechtl <mike.prechtl@ecsec.de>
2021-12-21 10:42:59 +0100

    Make it clear that the 'v' prefix is not required as input

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LATEST_TAG=$(git tag -l | tail -n 1)
 
 printf "Your latest release version: ${LATEST_TAG}\n"
+printf "Provide the new release version without the 'v' prefix.\n"
 read -p "What is the new release version (<EIDAS_NODE_VERSION>-<REVISION>): " NEW_TAG
 
 # Update version in helm chart
